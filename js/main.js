e944370c212856492df4c827feb9719d86679dd8
Your Name <n_fainer@fanshaweonline.ca>
2020-01-15 09:44:10 -0500

    trevor

--- js/main.js
+++ js/main.js
@@ -1,24 +1,42 @@
-import Vue from 'https://cdn.jsdelivr.net/npm/vue@2.6.11/dist/vue.esm.browser.js';
+// todo => use a key to track the current video, or just pass the video in as a ref to the function and grab its source
 
-// set up viewmodel and use th vue framework
-const my_vm = (() => {
-    // variables, or define components... whatever
+var vm = new Vue({
+  el: "#app",
 
-    let vue_VM = new Vue({
-        data: {
-            message: "Hello from Vue!",
+  data: {
 
-            collection: {
-                {name: "Joe", role:"Prof"},
-                {name: "John", role:"Prof"},
-                {name: "Jarrod", role:"Prof"}
-            }
-        },
+    // mock up the user - this well eventually come from the database UMS (user management system)
+    user: {
+      isAdmin: false,
+      avatar: 'thor.png',
+      isLoggedIn: true
+    },
 
-        methods: {
-            logClicked() {
-                console.log('clicked on an element!');
-            }
-        }
-    }).$mount('#app');
-})();
+    // this data would also come from the database, but we'll just mock it up for now
+    videodata: [
+      { name: "Star Wars The Force Awakens", thumb: "forceawakens.jpg", vidsource: "forceawakens.mp4", description: "yet another star wars movie" },
+      { name: "Stranger Things", thumb: "strangerthings.jpg", vidsource: "strangerthings.mp4", description: "don't get lost in the upside down" },
+      { name: "Marvel's The Avengers", thumb: "avengers.jpg", vidsource: "avengers.mp4", description: "will they make black widow action figures this time?" }
+    ],
+
+    showDetails: false,
+
+  },
+
+  methods: {
+    setUserPrefs() {
+      // this is the preferences control, hit the api when ready (or use a component)
+      console.log('set user prefs here');
+    },
+
+    userLogIn() {
+      // call the login route, and / or load the login component
+      console.log('do login/ logout on click');
+
+      // this is a ternary statement -> shorthand for if / else
+      // the expression evaluates to treu or false - if its true, set the value equal to
+      // the left of the colon. if its false, set the value equal to the right.
+      this.user.isLoggedIn = (this.user.isLoggedIn) ? false : true;
+    }
+  }
+});
